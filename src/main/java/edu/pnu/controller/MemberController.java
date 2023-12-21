e2f95623f39f011d74603166e10b3a0fa3df994e
ORENOL <toppmy67@gmail.com>
2023-12-21 15:13:12 +0900

    회원 비밀번호 변경, 회원 권한 변경 API 구현

---
+++ src/main/java/edu/pnu/controller/MemberController.java
@@ -0,0 +1,43 @@
+package edu.pnu.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.pnu.domain.Member;
+import edu.pnu.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/private/member")
+@Tag(name = "멤버 컨트롤러", description = "계정 관리 모듈")
+public class MemberController {
+	
+	private MemberService memberService;
+	
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	@Operation(summary = "회원 비밀번호 변경", description = "member 객체에는 password 프로퍼티만 입력하면 됩니다.")
+	@PutMapping("changePassword")
+	public ResponseEntity<?> changePassword(@RequestBody Member member, Authentication authentication) {
+		return memberService.changePassword(member, authentication);
+	}
+	
+	@Secured("ROLE_ADMIN")
+	@Operation(summary = "타회원 권한 변경", description = "해당 api는 admin 권한을 필요로 합니다.<br>member 객체에는 username 프로퍼티만 입력하면 됩니다.")
+	
+	@PutMapping("changeAuthority")
+	public ResponseEntity<?> changeAuthority(@RequestBody Member member, Authentication authentication) {
+		return memberService.changeAuthority(member, authentication);
+	}
+	
+
+}
---
+++ src/main/java/edu/pnu/service/MemberService.java
@@ -0,0 +1,62 @@
+package edu.pnu.service;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import edu.pnu.domain.Member;
+import edu.pnu.domain.Role;
+import edu.pnu.persistence.MemberRepository;
+
+@Service
+public class MemberService {
+	
+	private MemberRepository memberRepo;
+	
+	private PasswordEncoder encoder;
+	
+	public MemberService(MemberRepository memberRepo, PasswordEncoder encoder) {
+		this.memberRepo = memberRepo;
+		this.encoder = encoder;
+	}
+
+	public ResponseEntity<?> changePassword(Member member, Authentication authentication) {
+		
+		try {
+			Optional<Member> existMember = memberRepo.findById(authentication.getName());
+			Member tempMember = Member.builder()
+					.username(authentication.getName())
+					.password(encoder.encode(member.getPassword()))
+					.role(existMember.get().getRole())
+					.build();
+			memberRepo.save(tempMember);
+			return ResponseEntity.ok("password change success");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("unexpected error occurs: " + e.getMessage());
+		}
+
+	}
+
+	public ResponseEntity<?> changeAuthority(Member member, Authentication authentication) {
+		try {
+			Optional<Member> existMember = memberRepo.findById(authentication.getName());
+			Member tempMember = Member.builder()
+					.username(member.getUsername())
+					.password(existMember.get().getPassword())
+					.role(member.getRole())
+					.build();
+			memberRepo.save(tempMember);
+			return ResponseEntity.ok("권한 변경");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("unexpected error occurs: " + e.getMessage());
+		}
+
+	}
+
+}
