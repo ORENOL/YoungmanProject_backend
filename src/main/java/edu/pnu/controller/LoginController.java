8f984a39f4b903e2c0dd433ff44a20bb4b2277cb
ORENOL <toppmy67@gmail.com>
2023-12-21 11:38:57 +0900

    Swagger 테스트 시 JWT 인증 기능 추가

--- src/main/java/edu/pnu/controller/LoginController.java
+++ src/main/java/edu/pnu/controller/LoginController.java
@@ -1,10 +1,18 @@
 package edu.pnu.controller;
 
+import java.util.Date;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import edu.pnu.domain.Member;
 import edu.pnu.persistence.MemberRepository;
@@ -27,12 +35,26 @@ public class LoginController {
 		this.encoder = encoder;
 	}
 	
+	@Operation(summary = "로그인 시 404 Not Found 방지를 위한 API")
 	@PostMapping("/login")
 	public ResponseEntity<?> login(){
 		return ResponseEntity.ok("ok");
 	}
 	
-	@Operation(summary = "사용자 회원가입 기능입니다.")
+	@Operation(summary = "Swagger 테스트용 JWT 발급 API", description = "Try it out -> Execute 이후 발급된 JWT코드를 우측 상단의 Authorize에 입력하세요.")
+	@PutMapping("/provideJWT")
+	public ResponseEntity<?> provideJWT() {
+
+		String token = JWT.create()
+				.withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+				.withClaim("username", "test1")
+				.sign(Algorithm.HMAC256("edu.pnu.jwt"));
+		
+		return ResponseEntity.ok("Bearer " + token);
+	}
+	
+	
+	@Operation(summary = "사용자 회원가입 기능")
 	@PostMapping("/api/public/signup")
 	public ResponseEntity<?> signup(@RequestBody Member member){
 		
@@ -52,7 +74,7 @@ public class LoginController {
 		return ResponseEntity.badRequest().build();
 	}
 	
-	@Operation(summary = "회원가입시 아이디 중복체크 기능입니다.") 
+	@Operation(summary = "회원가입시 아이디 중복체크 기능") 
 	@PostMapping("/api/public/doubleCheck")
 	public ResponseEntity<?> doubleCheck(@RequestBody Member member) {
 		return loginService.doubleCheck(member);
