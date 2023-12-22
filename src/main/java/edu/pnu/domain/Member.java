2ffe7a5887c6ad63802dda650456350cadfab401
ORENOL <toppmy67@gmail.com>
2023-12-22 14:10:17 +0900

    회원 ID 찾기 기능용 프로퍼티 email 추가

--- src/main/java/edu/pnu/domain/Member.java
+++ src/main/java/edu/pnu/domain/Member.java
@@ -3,12 +3,10 @@ package edu.pnu.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
@@ -23,4 +21,5 @@ public class Member {
 	String username;
 	String password;
 	private Role role;
+	String email;
 }
--- src/main/java/edu/pnu/persistence/MemberRepository.java
+++ src/main/java/edu/pnu/persistence/MemberRepository.java
@@ -3,7 +3,11 @@ package edu.pnu.persistence;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import edu.pnu.domain.Member;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface MemberRepository extends MongoRepository<Member, String> {
+	Optional<Member> findByEmail(String email);
 
 }
